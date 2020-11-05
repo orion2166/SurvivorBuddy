4bc40ce6e579e83fd0fc8ee852b7676597986ddb
NooneKot <roycemingyu@tamu.edu>
2020-11-05 12:19:08 +0800

    test

--- tests/ApplicationTests.py
+++ tests/ApplicationTests.py
@@ -8,27 +8,26 @@ from unittest.mock import patch
 from Application import Application
 
 class ApplicationMethods(unittest.TestCase):
+    def test_Application():
+        
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_create_widgets():
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_update_image():
 
-    def test_Application(self):
-        root = tk.Tk()
-        app = Application(master=root)
-        Application.create_widgets.assert_called_once()
-        #Application.create_widgets.create_menu.assert_called_once()
-        #Application.create_widgets.update_image.assert_called()
+
+    def create_menu():
+
+
+    def refresh_devices():
+
+
+    def connect():
+
+
+    def close():
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests/CamThreadTests.py
+++ tests/CamThreadTests.py
@@ -7,21 +7,6 @@ from CamThread import camPreview
 from CamThread import camThread
 
 class TestCamThreadMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
     def test_camPreview(self):
         cam = Mock()
         test = camThread("Survivor Cam", 1, cam)
